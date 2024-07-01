396c44ff284472ec15932e7226d51e930a053ff6
Adonaï Massa <massaadonai@gmail.com>
2024-07-01 13:12:28 +0200

    initial import

---
+++ js/App.js
@@ -0,0 +1,95 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Formulaire soumis !');
+});
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function() {
+      // Remove active class from all articles
+      document.querySelectorAll('.page article').forEach(article => {
+          article.classList.remove('active');
+      });
+
+      // Get the page name from the clicked link
+      const page = this.getAttribute('data-page');
+
+      // Add active class to the corresponding article
+      document.querySelector(`.page article[data-page="${page}"]`).classList.add('active');
+  });
+});
+
+document.querySelectorAll('.filter-list button').forEach(button => {
+  button.addEventListener('click', function() {
+      const category = this.innerText.toLowerCase();
+      
+      document.querySelectorAll('.filter-list button').forEach(btn => btn.classList.remove('active'));
+      this.classList.add('active');
+      
+      document.querySelectorAll('.project-item').forEach(item => {
+          if (category === 'all' || item.dataset.category.toLowerCase() === category) {
+              item.classList.add('active');
+          } else {
+              item.classList.remove('active');
+          }
+      });
+  });
+});
+
+document.querySelector('.filter-select').addEventListener('click', () => {
+  document.querySelector('.select-list').classList.toggle('active');
+});
+
+document.querySelectorAll('.select-item button').forEach(button => {
+  button.addEventListener('click', function() {
+      const category = this.innerText.toLowerCase();
+      document.querySelector('.select-value').innerText = this.innerText;
+      
+      document.querySelector('.select-list').classList.remove('active');
+      
+      document.querySelectorAll('.project-item').forEach(item => {
+          if (category === 'all' || item.dataset.category.toLowerCase() === category) {
+              item.classList.add('active');
+          } else {
+              item.classList.remove('active');
+          }
+      });
+  });
+});
+
+
+document.querySelectorAll('.select-item button').forEach(button => {
+  button.addEventListener('click', function() {
+      const category = this.innerText.toLowerCase();
+      document.querySelector('.select-value').innerText = this.innerText;
+      
+      document.querySelector('.select-list').classList.remove('active');
+      
+      document.querySelectorAll('.skills-item').forEach(item => {
+          if (category === 'all' || item.dataset.category.toLowerCase() === category) {
+              item.classList.add('active');
+          } else {
+              item.classList.remove('active');
+          }
+      });
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const filterButtons = document.querySelectorAll('[data-filter-btn]');
+  const skillItems = document.querySelectorAll('[data-filter-item]');
+
+  filterButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          const category = button.getAttribute('data-filter-btn');
+          
+          skillItems.forEach(item => {
+              item.classList.toggle('active', item.getAttribute('data-category') === category);
+          });
+      });
+  });
+});
+
+
+
+
+
